17c5ca9d83f6f104290cb717e344d451e8e41c95
alnomancs <alnoman.cs@gmail.com>
2022-05-07 16:03:54 +0100

    for heroku deploy

--- __server_deploy.js
+++ __server_deploy.js
@@ -7,4 +7,13 @@
  * 3. install heroku cli
  * 4. heroku login
  * 5.
+ *
+ *
+ * ---------------------------
+ * For each project on time
+ * ---------------------------
+ * 1. heroku create
+ * 2. make sure you : git add. git commit. git push.
+ * 3. git push heroku main
+ * 
  * */
